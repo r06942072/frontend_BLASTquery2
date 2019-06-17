a524ae819671e11a56d7d14030b874a5073305f4
Chun-Hung <32384566+r06942072@users.noreply.github.com>
2019-06-17 16:13:47 -0400

    add unittest to OurDb

--- src/components/OurDb/unit.test.js
+++ src/components/OurDb/unit.test.js
@@ -4,14 +4,12 @@ import { shallow, mount } from 'enzyme';
 //
 import OurDb from './';
 
-/* example
 describe('test suite of one react component', () => {
-	it('test 1', () => {
+	it('should render snapshot', () => {
+		const wrapper = shallow(<OurDb />);
 		expect(wrapper.exists()).toBe(true);
 	});
 	it('test 2', () => {
-		const text = wrapper.find('p').text();
-		expect(text).toEqual('count is 0');
+
 	});
 });
-*/
