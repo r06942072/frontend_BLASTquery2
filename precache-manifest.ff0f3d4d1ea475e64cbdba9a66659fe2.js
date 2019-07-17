656f7dc41d691bac7b68d8c6c7d3c300144df12c
Surface Pro 6, linux <johnbamboobilly@DESKTOP-8J948KJ.localdomain>
2019-07-17 01:07:11 -0400

    Updates

---
+++ precache-manifest.ff0f3d4d1ea475e64cbdba9a66659fe2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f97ff7a9108646e0170bf32f1f7bf26",
+    "url": "/frontend_BLASTquery/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/frontend_BLASTquery/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0499f6f7e57839626ee5",
+    "url": "/frontend_BLASTquery/static/js/main.cfab9618.chunk.js"
+  },
+  {
+    "revision": "3428618f7d8e38b566ca",
+    "url": "/frontend_BLASTquery/static/js/runtime~main.8d19b23a.js"
+  }
+]);
